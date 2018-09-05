c940560ac67ffe8f150f9a494ee672df17e7046b
Heetmadhu <madhuheet1997@gmail.com>
2018-09-05 08:06:32 +0530

    first commit -Heet

---
+++ app/src/main/java/silencer/apps/technobugs/com/silencer/SilencerAppActivity.kt
@@ -0,0 +1,12 @@
+package silencer.apps.technobugs.com.silencer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SilencerAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_silencer_app)
+    }
+}
