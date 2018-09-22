993919ab5f416853500e30ba8dda2aab14b97d98
Heetmadhu <madhuheet1997@gmail.com>
2018-09-22 20:01:07 +0530

    Added Utils

--- app/src/main/java/silencer/apps/technobugs/com/silencer/utils/LogUtils.java
+++ app/src/main/java/silencer/apps/technobugs/com/silencer/utils/LogUtils.java
@@ -5,41 +5,47 @@ import android.util.Log;
 import silencer.apps.technobugs.com.silencer.BuildConfig;
 
 public class LogUtils {
-    private String tag;
+    private String TAG;
     private boolean isDebugMode;
+    private static LogUtils instance;
 
-    LogUtils(String tag){
-        this.tag = tag;
-        this.isDebugMode = BuildConfig.DEBUG;
+    public static LogUtils getInstance(String tag){
+        if(instance == null){
+            instance = new LogUtils();
+            instance.isDebugMode = BuildConfig.DEBUG;
+        }
+        instance.TAG = tag;
+        return instance;
     }
+
 
     void d(String msg){
         if(isDebugMode){
-            Log.d(tag,msg);
+            Log.d(TAG,msg);
         }
     }
 
     void w(String msg){
         if(isDebugMode){
-            Log.w(tag,msg);
+            Log.w(TAG,msg);
         }
     }
 
     void v(String msg){
         if(isDebugMode){
-            Log.v(tag,msg);
+            Log.v(TAG,msg);
         }
     }
 
     void e(String msg){
         if(isDebugMode){
-            Log.e(tag,msg);
+            Log.e(TAG,msg);
         }
     }
 
     void i(String msg){
         if(isDebugMode){
-            Log.i(tag,msg);
+            Log.i(TAG,msg);
         }
     }
 
